4f492dd835532a885d2e7f586cdbafa22cb46de0
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2018-04-21 13:41:18 -0300

    Construção do primeiro componente

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,11 +22,11 @@ module.exports = {
         ],
         "linebreak-style": [
             "error",
-            "windows"
+            "unix"
         ],
         "quotes": [
             "error",
-            "double"
+            "single"
         ],
         "semi": [
             "error",
