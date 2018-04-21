5fce7ec5f5adb37a17b035b695824faf59244e5e
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2018-04-21 17:21:44 -0300

    Criação do componente de lista de releases

--- src/container/Releases.js
+++ src/container/Releases.js
@@ -1,8 +1,52 @@
 import React, { Component } from "react";
 
 class Releases extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			releases: [
+				{ id: 1, nomeRelease: "Projeto inicial", dataRelease: "22/01/2017" },
+				{
+					id: 2,
+					nomeRelease: "Inclusão de tela de assuntos",
+					dataRelease: "15/07/2017"
+				},
+				{
+					id: 3,
+					nomeRelease: "Concluão do módulo administrativo",
+					dataRelease: "03/09/2018"
+				}
+			]
+		};
+	}
+
 	render() {
-		return <h1>Releases</h1>;
+		return (
+			<table className="table">
+				<thead>
+					<tr>
+						<th scope="col">ID</th>
+						<th scope="col">Nome da Release</th>
+						<th scope="col">Data da Release</th>
+						<th scope="col" />
+					</tr>
+				</thead>
+				<tbody>
+					{this.state.releases.map((release, index) => {
+						return (
+							<tr key={release.id}>
+								<th scope="row">{release.id}</th>
+								<td>{release.nomeRelease}</td>
+								<td>{release.dataRelease}</td>
+								<td>
+									<button className="btn btn-danger btn-sm">Excluir</button>
+								</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		);
 	}
 }
 
